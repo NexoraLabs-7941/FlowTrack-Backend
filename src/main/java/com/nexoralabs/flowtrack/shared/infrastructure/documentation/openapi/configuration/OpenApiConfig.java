ba0bfa53c2aacc0ff04294a74b0fc005197011e5
Darksens <scg02040@gmail.com>
2026-05-09 23:18:25 -0500

    ajustes

--- src/main/java/com/nexoralabs/flowtrack/shared/infrastructure/documentation/openapi/configuration/OpenApiConfig.java
+++ src/main/java/com/nexoralabs/flowtrack/shared/infrastructure/documentation/openapi/configuration/OpenApiConfig.java
@@ -5,11 +5,8 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -19,14 +16,6 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-            .servers(List.of(
-//                new Server()
-//                    .url("https://flowtrack-backend-production.up.railway.app")
-//                    .description("Production Server"),
-                new Server()
-                    .url("http://localhost:8080")
-                    .description("Local Development Server")
-            ))
             .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
             .components(new Components()
                 .addSecuritySchemes(SECURITY_SCHEME_NAME,
